7de3e5602b657d3770cc1db7c77949671c8465bb
Tawana Howard <tawanahow@gmail.com>
2020-05-28 22:55:38 -0400

    add app.js refactor index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-const Index = () => {
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+
+function Index () {
   return (
-    <div>
-      <p>Sample app</p>
-    </div>
-  );
-};
+    <Page>
+      <Layout>
+        <EmptyState
+            heading="Manage your inventory transfers"
+            action={{content: 'Select Products'}}
+            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg">
+          <p>Select Products</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  )
+}
 
 export default Index;
+
