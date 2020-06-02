10f9d26586c1f8a95f5a371e08d4a254387955fc
Tawana Howard <tawanahow@gmail.com>
2020-06-01 23:54:35 -0400

    refacter project

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import { EmptyState, Layout, Page } from '@shopify/polaris';
-
-
-function Index () {
+const Index = () => {
   return (
-    <Page>
-      <Layout>
-        <EmptyState
-            heading="Manage your inventory transfers"
-            action={{content: 'Select Products'}}
-            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg">
-          <p>Select Products</p>
-        </EmptyState>
-      </Layout>
-    </Page>
+    <div>
+      <p>Sample Shopify App</p>
+    </div>
   )
 }
 
-export default Index;
-
+export default Index
